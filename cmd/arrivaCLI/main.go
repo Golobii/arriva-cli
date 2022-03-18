e8bfe1988ac3994e7d7a51671c34d8f5d599c58d
golobii <gcluka@gmail.com>
2022-03-18 15:53:32 +0100

    improved structure 2

--- cmd/arrivaCLI/main.go
+++ cmd/arrivaCLI/main.go
@@ -4,8 +4,10 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"strings"
 	"time"
 
+	ui "arriva-cli/internal/ui"
 	"arriva-cli/pkg/api"
 	"arriva-cli/pkg/auth"
 )
@@ -29,45 +31,40 @@ func main() {
 	case "date":
 		fmt.Println("Not implemented yet.")
 		return
+	case "help":
+		fmt.Println("Not implemented yet.")
+		return
 	default:
-		res := api.FetchDepartures(timeStamp, tok, strconv.Itoa(GetStationByName(os.Args[1], ds)),
-			strconv.Itoa(GetStationByName(os.Args[2], ds)), fmt.Sprintf("%d-%s", time.Now().Year(), os.Args[3]))
-		PrettyPrintDepartures(res)
+		handleSearch(ds, timeStamp, tok)
 		break
 	}
 
 }
 
-// TODO: clean this file
-
-func PrettyPrintDepartures(departures api.DeparturesResponse) {
-	fmt.Println("2022-03-16")
-	for _, departure := range departures[0].Departures {
-		fmt.Printf("*- %s -- %s\n", "Mengeš Lovec", departure.RodIodh) // TODO: add right names
-		fmt.Println("│  ...")
-		fmt.Printf("*- %s -- %s\n", "Kamnik Metalka", departure.RodIpri)
-		fmt.Printf("Trajanje: %s\n\n", formatTime(departure.RodCas))
+func handleSearch(ds api.DepartureStations, timeStamp string, tok string) {
+	if len(os.Args) < 4 {
+		fmt.Println("Not enough arguments supplied! Try: arrivaCLI help")
+		os.Exit(1)
 	}
+	res := api.FetchDepartures(timeStamp, tok, strconv.Itoa(getStationByName(os.Args[1], ds)),
+		strconv.Itoa(getStationByName(os.Args[2], ds)), fmt.Sprintf("%d-%s", time.Now().Year(), os.Args[3]))
+
+	if res[0].Error != "0" {
+		fmt.Println("No routes found.")
+		os.Exit(0)
+	}
+	ui.PrettyPrintDepartures(res, os.Args[1], os.Args[2])
+
 }
 
-func GetStationByName(name string, ds api.DepartureStations) int {
+func getStationByName(name string, ds api.DepartureStations) int {
 
+	name = strings.ToLower(name)
 	for _, station := range ds[0].DepartureStations {
-		if station.PosNaz == name {
+		if strings.ToLower(station.PosNaz) == name {
 			return station.JposIjpp
 		}
 	}
 
 	return -1
-}
-
-// Formats the length of a bus ride from int to str (7 => 00:07)
-func formatTime(time int) string {
-	hours := time / 60
-	minutes := time - hours*60
-
-	if hours < 10 {
-		return fmt.Sprintf("0%d:%d", hours, minutes)
-	}
-	return fmt.Sprintf("%d:%d", hours, minutes)
 }
---
+++ internal/ui/ui.go
@@ -0,0 +1,28 @@
+package console
+
+import (
+	"arriva-cli/pkg/api"
+	"fmt"
+)
+
+func PrettyPrintDepartures(departures api.DeparturesResponse, startStation string, endStation string) {
+	// TODO: display the right date
+	fmt.Println("2022-03-16")
+	for _, departure := range departures[0].Departures {
+		fmt.Printf("*- %s -- %s\n", startStation, departure.RodIodh) // TODO: add right names
+		fmt.Println("│  ...")
+		fmt.Printf("*- %s -- %s\n", endStation, departure.RodIpri)
+		fmt.Printf("Trajanje: %s\n\n", formatTime(departure.RodCas))
+	}
+}
+
+// Formats the length of a bus ride from int to str (7 => 00:07)
+func formatTime(time int) string {
+	hours := time / 60
+	minutes := time - hours*60
+
+	if hours < 10 {
+		return fmt.Sprintf("0%d:%d", hours, minutes)
+	}
+	return fmt.Sprintf("%d:%d", hours, minutes)
+}
